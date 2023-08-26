7dd13b18e74c2421ed4fcd13e19f01e20b86a31c
ayaka kato <ayajiayun@gmail.com>
2023-08-26 21:09:57 +0900

    ホーム画面ルート設定

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::middleware(['auth'])->group(function () {
 
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/home', [App\Http\Controllers\FoodRecordController::class, 'showInfo'])->name('home');
 
     // 【レシピ関連】
