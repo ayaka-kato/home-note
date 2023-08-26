bee8fe1c22bf2a50df04fe7ccbdab59aa46f3daf
ayaka kato <ayajiayun@gmail.com>
2023-08-26 21:25:01 +0900

    ホーム画面遷移 修正

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Auth::routes();
 
 Route::middleware(['auth'])->group(function () {
 
-    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::get('/home', [App\Http\Controllers\FoodRecordController::class, 'showInfo'])->name('info');
+    Route::get('/', [App\Http\Controllers\FoodRecordController::class, 'showInfo'])->name('home');
+    // Route::get('/home', [App\Http\Controllers\FoodRecordController::class, 'showInfo'])->name('home');
 
     // 【レシピ関連】
     // -------------------------------------------------------------------------------------------------------------
